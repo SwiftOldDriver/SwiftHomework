014195da44d4b1824bae8f2918ec1ad6d18c2ebc
Jiar <jiar.world@gmail.com>
2016-11-28 10:44:31 +0800

    Project 10 - VideoBackground

---
+++ Project 10 - VideoBackground/SpotifyVideoBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyVideoBackground
+//
+//  Created by Jiar on 2016/11/28.
+//  Copyright © 2016年 Jiar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
