e35a327963797184a9e4d2d948218b436f1b1258
Damonwong <coderonevv@gmail.com>
2016-11-28 15:56:55 +0800

    init Project 11 - ClearTableViewCell

---
+++ Project 11 - ClearTableViewCell/ClearTableViewCell/ClearTableViewCell/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ClearTableViewCell
+//
+//  Created by Damon on 2016/11/28.
+//  Copyright © 2016年 swiftdrive. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
+
