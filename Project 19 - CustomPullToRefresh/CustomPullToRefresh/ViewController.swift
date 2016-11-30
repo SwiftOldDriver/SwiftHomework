68d84c62688fa51b0f4dbe529b70a3b9dc335255
Yasin <yasinzhou@qq.com>
2016-11-30 14:19:29 +0800

    Project 19 Commint

--- Project 19 - CustomPullToRefresh/CustomPullToRefresh/ViewController.swift
+++ Project 19 - CustomPullToRefresh/CustomPullToRefresh/ViewController.swift
@@ -7,19 +7,167 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let dataArray = ["😂", "🤗", "😳", "😌", "😊"]
+    
+    let refreshController = UIRefreshControl()
+    var customView:UIView!
+    var isAnimating = false
+    
+    var labelsArray: Array<UILabel> = []
+    var currentColorIndex = 0
+    var currentLabelIndex = 0
+    
+    var timer: Timer?
+    
+    let colorsArray: [UIColor] = [.magenta, .brown, .yellow, .red, .green, .blue, .orange]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 60
+        
+        refreshController.backgroundColor = UIColor.clear
+        refreshController.tintColor = UIColor.clear
+        tableView.addSubview(refreshController)
+        loadCustomRefreshContents()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 35)
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if refreshController.isRefreshing {
+            startRefresh(startRefreshing: {
+                doSomething()
+            })
+        }
+    }
+    
+    
+    func loadCustomRefreshContents() {
+        customView = Bundle.main.loadNibNamed("RefreshContents", owner: self, options: nil)?.last as! UIView
+        customView.frame = refreshController.bounds
+        
+        for i in 1 ... customView.subviews.count  {
+            if let label = customView.viewWithTag(i) as? UILabel{
+                labelsArray.append(label)
+            }
+        }
+        
+        refreshController.addSubview(customView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func doSomething() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(ViewController.endOfWork), userInfo: nil, repeats: true)
     }
-
-
+    
+    func endOfWork() {
+        endRefresh()
+        
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func startRefresh(startRefreshing:() -> ()) {
+        if !isAnimating {
+            animateRefreshStep1()
+            startRefreshing()
+        }
+        
+    }
+    
+    func endRefresh() {
+        refreshController.endRefreshing()
+    }
+    
+    
+    func animateRefreshStep1() {
+        
+        isAnimating = true
+        
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
+            
+            self.labelsArray[self.currentLabelIndex].transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_4))
+            self.labelsArray[self.currentLabelIndex].textColor = self.getCurrentColor()
+            
+        }, completion: { (finished) -> Void in
+            
+            UIView.animate(withDuration: 0.05, delay: 0.0, options: .curveLinear, animations: { () -> Void in
+                self.labelsArray[self.currentLabelIndex].transform = .identity
+                self.labelsArray[self.currentLabelIndex].textColor = .black
+                
+            }, completion: { (finished) -> Void in
+                if self.refreshController.isRefreshing {
+                    self.currentLabelIndex += 1
+                    
+                    if self.currentLabelIndex < self.labelsArray.count {
+                        self.animateRefreshStep1()
+                    } else {
+                        self.currentLabelIndex = 0
+                        self.animateRefreshStep2()
+                    }
+                } else {
+                    self.isAnimating = false
+                    self.currentLabelIndex = 0
+                }
+                
+            })
+        })
+    }
+    
+    func animateRefreshStep2() {
+        UIView.animate(withDuration: 0.40, delay: 0.0, options: .curveLinear, animations: { () -> Void in
+            
+            for label in self.labelsArray {
+                label.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            }
+            
+        }, completion: { (finished) -> Void in
+            
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveLinear, animations: { () -> Void in
+                for label in self.labelsArray {
+                    label.transform = .identity
+                }
+                
+            }, completion: { (finished) -> Void in
+                if self.refreshController.isRefreshing {
+                    self.animateRefreshStep1()
+                }
+                else {
+                    self.isAnimating = false
+                    for label in self.labelsArray {
+                        label.textColor = .black
+                        label.transform = .identity
+                    }
+                }
+            })
+        })
+    }
+    
+    func getCurrentColor() -> UIColor {
+        //获取当前颜色
+        let returnColor = colorsArray[currentColorIndex]
+        //设置下一个颜色的游标
+        currentColorIndex = (currentColorIndex + 1)%colorsArray.count
+        return returnColor
+    }
 }
+
 
