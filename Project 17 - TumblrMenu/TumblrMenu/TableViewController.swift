acd6a7fe350950894a4cbca9121e8c790472dec6
米广 <mig@dxyer.com>
2016-11-30 20:34:22 +0800

    搭建项目结构

---
+++ Project 17 - TumblrMenu/TumblrMenu/TableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  TableViewController.swift
+//  TumblrMenu
+//
+//  Created by Mee Leo on 11/29/16.
+//  Copyright © 2016 Mee Leo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBarItem()
+        
+    }
+
+    private func setupTabBarItem() {
+        let postImage = UIImage(imageLiteralResourceName: "newPost").withRenderingMode(.alwaysOriginal)
+        let tabBarItem = UITabBarItem(title: nil, image: postImage, selectedImage: nil)
+        self.tabBarItem = tabBarItem
+        self.tabBarController?.delegate = self
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier1", for: indexPath)
+            return cell
+        }
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 207
+        }else {
+            return 321
+        }
+    }
+    
+    
+    // MARK: - TabbarController Delegate
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+    }
+
+}
---
+++ Project 17 - TumblrMenu/TumblrMenu/TransitionManager.swift
@@ -0,0 +1,14 @@
+//
+//  TransitionManager.swift
+//  TumblrMenu
+//
+//  Created by Mee Leo on 11/29/16.
+//  Copyright © 2016 Mee Leo. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+
+    
+}
