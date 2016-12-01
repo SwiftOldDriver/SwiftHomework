01e4669945e08aafe7000ce26b9becae18ecb483
Yasin <yasinzhou@qq.com>
2016-12-01 16:12:57 +0800

    Project 19 update

--- Project 19 - CustomPullToRefresh/CustomPullToRefresh/ViewController.swift
+++ Project 19 - CustomPullToRefresh/CustomPullToRefresh/ViewController.swift
@@ -11,19 +11,25 @@ class ViewController: UIViewController,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let dataArray = ["😂", "🤗", "😳", "😌", "😊"]
+    let emojis = ["😂", "🤗", "😳", "😌", "😊"]
     
     let refreshController = UIRefreshControl()
     var customView: UIView!
     var isAnimating = false
     
-    var labelsArray: [UILabel] = []
-    var currentColorIndex = 0
+    var labelsArray = [UILabel]()
     var currentLabelIndex = 0
     
-    var timer: Timer?
-    
     let transformColors: [UIColor] = [.magenta, .brown, .yellow, .red, .green, .blue, .orange]
+    var currentColorIndex = 0
+    
+    lazy var currentColor: () -> UIColor = {
+        //获取当前颜色
+        let returnColor = self.transformColors[self.currentColorIndex]
+        //设置下一个颜色的游标
+        self.currentColorIndex = (self.currentColorIndex + 1) % self.transformColors.count
+        return returnColor
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,17 +42,17 @@ class ViewController: UIViewController,UITableViewDelegate {
         refreshController.backgroundColor = UIColor.clear
         refreshController.tintColor = UIColor.clear
         tableView.addSubview(refreshController)
+        
         loadCustomRefreshContents()
     }
     
-    
-    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         if refreshController.isRefreshing {
+//            startRefresh {doSomething()}  //可以使用尾随闭包的写法，也可以使用下面这种将函数作为参数传递
+            
             startRefresh(then: doSomething)
         }
     }
-    
     
     func loadCustomRefreshContents() {
         customView = Bundle.main.loadNibNamed("RefreshContents", owner: self, options: nil)?.last as! UIView
@@ -62,81 +68,64 @@ class ViewController: UIViewController,UITableViewDelegate {
     }
 
     func doSomething() {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(ViewController.finished), userInfo: nil, repeats: true)
-    }
-    
-    func finished() {
-        endRefresh()
-        
-        timer?.invalidate()
-        timer = nil
+        //模拟一个耗时操作
+        delay(delay: 5, closure: {
+            self.endRefresh()
+        })
     }
     
     func startRefresh(then:() -> ()) {
         if !isAnimating {
-            animateRefreshStep1()
+            transformColorAnimate()
             then()
         }
-        
     }
     
     func endRefresh() {
         refreshController.endRefreshing()
     }
     
-    
-    func animateRefreshStep1() {
-        
+    func transformColorAnimate() {
         isAnimating = true
         
         UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
-            
             self.labelsArray[self.currentLabelIndex].transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_4))
-            self.labelsArray[self.currentLabelIndex].textColor = self.getCurrentColor()
-            
+            self.labelsArray[self.currentLabelIndex].textColor = self.currentColor()
         }, completion: { (finished) -> Void in
-            
             UIView.animate(withDuration: 0.05, delay: 0.0, options: .curveLinear, animations: { () -> Void in
                 self.labelsArray[self.currentLabelIndex].transform = .identity
                 self.labelsArray[self.currentLabelIndex].textColor = .black
-                
             }, completion: { (finished) -> Void in
                 if self.refreshController.isRefreshing {
                     self.currentLabelIndex += 1
                     
                     if self.currentLabelIndex < self.labelsArray.count {
-                        self.animateRefreshStep1()
+                        self.transformColorAnimate()
                     } else {
                         self.currentLabelIndex = 0
-                        self.animateRefreshStep2()
+                        self.transformAllSizeAnimate()
                     }
                 } else {
                     self.isAnimating = false
                     self.currentLabelIndex = 0
                 }
-                
             })
         })
     }
     
-    func animateRefreshStep2() {
+    func transformAllSizeAnimate() {
         UIView.animate(withDuration: 0.40, delay: 0.0, options: .curveLinear, animations: { () -> Void in
-            
             self.labelsArray.forEach({ (label) in
                 label.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
             })
-            
         }, completion: { (finished) -> Void in
-            
             UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveLinear, animations: { () -> Void in
                 self.labelsArray.forEach({ (label) in
                     label.transform = .identity
                 })
-                
             }, completion: { (finished) -> Void in
                 if self.refreshController.isRefreshing {
-                    self.animateRefreshStep1()
+                    self.transformColorAnimate()
                 }
                 else {
                     self.isAnimating = false
@@ -149,22 +138,19 @@ class ViewController: UIViewController,UITableViewDelegate {
         })
     }
     
-    func getCurrentColor() -> UIColor {
-        //获取当前颜色
-        let returnColor = transformColors[currentColorIndex]
-        //设置下一个颜色的游标
-        currentColorIndex = (currentColorIndex + 1)%transformColors.count
-        return returnColor
+    func delay(delay:Int, closure: @escaping ()->()) {
+        let deadlineTime = DispatchTime.now() + .seconds(delay)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: closure)
     }
 }
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath)
-        cell.textLabel?.text = dataArray[indexPath.row]
+        cell.textLabel?.text = emojis[indexPath.row]
         cell.textLabel?.font = UIFont.systemFont(ofSize: 35)
         cell.textLabel?.textAlignment = .center
         return cell
